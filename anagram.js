8ba41a84d680624c823e733275df3c60e50ee777
NZX\kira.bialczynska <kira.bialczynska@nzx.com>
2016-09-23 14:53:59 +1200

    Angaram base functions finished

--- anagram.js
+++ anagram.js
@@ -1,13 +1,7 @@
-var wordsList = ["mleko", "doniczka", "komputer", "filiżanka"]
-
-/*function Game(){
-   this.createAnagram = createAnagram();
-   this.displayAnagram = displayAnagram();
-   //this.compare = compare();   
-}
-
-var game1 = new Game();*/
+var wordsList = ["herbata", "żelazko", "matematyka", "tyczka", "kocur", "rzepa", "homologacja", "hemoglobina"]; 
+//["mleko", "doniczka", "komputer", "chusteczki", "nocnik", "koszula", "inwestygacja","algorytm", "dupa", "dziunia", "smoczek", "papierosy","wiosna"];
 var letters = [];
+var word;
 
 function pickRandomNumber(rangeStart, rangeEnd) {
     var randomNumber = rangeStart + Math.floor(Math.random() * (rangeEnd - (rangeStart + 1)));
@@ -16,14 +10,14 @@ function pickRandomNumber(rangeStart, rangeEnd) {
 function mixLetters() {
     for (j = 0; j < letters.length; j++) {
         var letterToReplace = letters[j];
-        var randomNumber = pickRandomNumber(j, letters.length);
+        var randomNumber = pickRandomNumber(j + 1, letters.length);
         var letterToReplaceWith = letters[randomNumber];
         letters[j] = letterToReplaceWith;
         letters[randomNumber] = letterToReplace;             
         }  
 }
 function createAnagram(){
-       var word = wordsList[Math.floor(Math.random() * wordsList.length)];       
+       word = wordsList[Math.floor(Math.random() * wordsList.length)];       
        for (i = 0; i < word.length; i++) {
           letters.push(word[i]);                     
        }; 
@@ -33,11 +27,45 @@ function createAnagram(){
    }
 function displayAnagram(){
          var selectedWord = createAnagram();
-         $('.anagram').append(selectedWord);    
+         $('.anagram h2').append(selectedWord);    
+}
+function compareResult(){
+    var userInput = $('.answer input').val();
+    $('.output h3').empty();
+    if (userInput == word) {
+        $('.output h3').append('You win!');
+        $('.win').addClass('visible');
+        $('.lose').removeClass('visible');
+    }
+    else {
+        $('.output h3').append('Loser!');
+        $('.lose').addClass('visible');
+        $('.win').removeClass('visible');
+    }
+}
+function cleanAnagram(){
+    $('.anagram h2').empty();
+    $('.output h3').empty();
+    $('.answer input').val('');
+    $('.win').removeClass('visible');
+    $('.lose').removeClass('visible');
+    word = '';
+    letters= [];
+}
+function newGame() {
+    cleanAnagram();
+    displayAnagram(); 
+    $('.answer').submit(function(){
+        compareResult();
+    });
 }
 $(document).ready(function(){   
-   displayAnagram(); 
+  newGame(); 
+  $('#refresh').click(function(){
+        newGame(); 
+    });   
 })
+
 
 
 
