35f4040b44b1e243f771a82e8f96ae64fd3a3369
kirunia <kira.bialczynska@gmail.com>
2016-09-23 17:16:31 +1200

    Base objects
    Base objects created for Round and Anagram

--- anagram.js
+++ anagram.js
@@ -1,58 +1,76 @@
-var wordsList = ["herbata", "żelazko", "matematyka", "tyczka", "kocur", "rzepa", "homologacja", "hemoglobina"]; 
-//["mleko", "doniczka", "komputer", "chusteczki", "nocnik", "koszula", "inwestygacja","algorytm", "dupa", "dziunia", "smoczek", "papierosy","wiosna"];
-var letters = [];
-var word;
+var wordsList = ["mleko", "doniczka", "komputer", "chusteczki", "nocnik", "koszula", "inwestygacja","algorytm", "dupa", "dziunia", "smoczek", "papierosy","wiosna"];
 
-function pickRandomNumber(rangeStart, rangeEnd) {
-    var randomNumber = rangeStart + Math.floor(Math.random() * (rangeEnd - (rangeStart + 1)));
+
+//class definition for an anagram
+class Anagram {
+    constructor(word){         
+        this.word = word;
+        this.letters = [];
+    }
+    pickRandomNumber(rangeStart, rangeEnd) {
+    let randomNumber = rangeStart + Math.floor(Math.random() * (rangeEnd - (rangeStart + 1)));
     return randomNumber;
-}
-function mixLetters() {
-    for (j = 0; j < letters.length; j++) {
-        var letterToReplace = letters[j];
-        var randomNumber = pickRandomNumber(j + 1, letters.length);
-        var letterToReplaceWith = letters[randomNumber];
-        letters[j] = letterToReplaceWith;
-        letters[randomNumber] = letterToReplace;             
+    }
+    mixLetters() {
+    for (var j = 0; j < this.letters.length; j++) {
+        let letterToReplace = this.letters[j];
+        let randomNumber = this.pickRandomNumber(j + 1, this.letters.length);
+        let letterToReplaceWith = this.letters[randomNumber];
+        this.letters[j] = letterToReplaceWith;
+        this.letters[randomNumber] = letterToReplace;             
         }  
-}
-function createAnagram(){
-       word = wordsList[Math.floor(Math.random() * wordsList.length)];       
-       for (i = 0; i < word.length; i++) {
-          letters.push(word[i]);                     
+    }
+    createAnagram(){
+       this.word = wordsList[Math.floor(Math.random() * wordsList.length)];       
+       for (let initialLetter of this.word) {
+          this.letters.push(initialLetter);                     
        }; 
-       mixLetters(); 
-       selectedWord = letters.join('');
+       this.mixLetters(); 
+       let selectedWord = this.letters.join('');
        return selectedWord;                 
-   }
-function displayAnagram(){
-         var selectedWord = createAnagram();
-         $('.anagram h2').append(selectedWord);    
+   }   
 }
-function compareResult(){
-    var userInput = $('.answer input').val();
-    $('.output h3').empty();
-    if (userInput == word) {
-        $('.output h3').append('You win!');
-        $('.win').addClass('visible');
-        $('.lose').removeClass('visible');
+
+//class definition for game rounds
+class Round {
+    constructor(anagram,targetToDisplay,userInput,outputTarget, winScreen,loseScreen) {
+        this.anagram = anagram;
+        this.targetToDisplay = targetToDisplay;
+        this.userInput = userInput;
+        this.outputTarget = outputTarget;
+    }
+    cleanScreen(){
+        $(this.targetToDisplay).empty();
+        $(this.outputTarget).empty();
+        $(this.userInput).val('');
+        $(this.winScreen).removeClass('visible');
+        $(this.loseScreen).removeClass('visible');
+    }    
+    compareResult(){
+    let userInputValue = this.userInput.val();
+    $(this.outputTarget).empty();
+    if (userInputValue == word) {
+        $(this.outputTarget).append('You win!');
+        $(this.winScreen).addClass('visible');
+        $(this.loseScreen).removeClass('visible');
     }
     else {
-        $('.output h3').append('Loser!');
-        $('.lose').addClass('visible');
-        $('.win').removeClass('visible');
+        $(this.outputTarget).append('Loser!');
+        $(this.loseScreen).addClass('visible');
+        $(this.winScreen).removeClass('visible');
     }
+ }
 }
-function cleanAnagram(){
-    $('.anagram h2').empty();
-    $('.output h3').empty();
-    $('.answer input').val('');
-    $('.win').removeClass('visible');
-    $('.lose').removeClass('visible');
-    word = '';
-    letters= [];
-}
-function newGame() {
+
+const anagram1 = new Anagram()
+const round1 = new Round();
+
+
+
+
+
+
+/*function newGame() {
     cleanAnagram();
     displayAnagram(); 
     $('.answer').submit(function(){
@@ -64,7 +82,7 @@ $(document).ready(function(){
   $('#refresh').click(function(){
         newGame(); 
     });   
-})
+})*/
 
 
 
