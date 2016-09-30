e9f9e3a2a6c8f37c12a10459422547bb989174f6
kirunia <kira.bialczynska@gmail.com>
2016-09-30 14:44:48 +1300

    Round class separated

--- application.js
+++ application.js
@@ -1,39 +1,3 @@
-class Round {
-    constructor(anagram, roundScore, answerScore) {        
-        this.anagram = anagram;
-        this.answerScore = answerScore;        
-        this.reshuffleCounter = 0;
-        this.roundScore = roundScore;
-    }    
-    calculateResult(valueToCompare) {
-        let wordLength = this.anagram.mixedWord.length;
-        let result = this.anagram.compareWords(valueToCompare); 
-        if (result == true) {
-            this.answerScore = this.answerScore + 10 * wordLength;
-        }
-        else {
-            this.answerScore = this.answerScore - 20;
-        }       
-        return result;
-    }
-   
-    reshuffleWord() {       
-        let mixedWord = this.anagram.createAnagram();
-        this.reshuffleCounter = this.reshuffleCounter + 1; 
-        return mixedWord;       
-    }
-    replayAnagram() {
-        this.reshuffleCounter = 0;
-        this.roundScore = 0;
-        let mixedWord = this.anagram.createAnagram();
-        return mixedWord;
-    }
-    calculateRoundScore(){
-        this.roundScore = this.roundScore + this.answerScore - this.reshuffleCounter * 10;
-        return this.roundScore;
-    }
-}
-
 $(document).ready(function(){
     let anagram1 = new Anagram(pickWord());
     let round1 = new Round(anagram1);
